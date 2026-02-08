c80656b38b2a18bbe8d4ae9218bf20cd7d42754c
Serghei_Granici <granici.design@gmail.com>
2026-02-08 16:45:57 +0100

    Task 02

---
+++ React-HW_03-v2.0/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import List from './components/list';
+
+function App() {
+  return (
+    <>
+      <List />
+    </>
+  );
+}
+
+export default App;
---
+++ React-HW_03-v2.0/src/components/list/index.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+function List() {
+  const [people, setPeople] = useState([
+    { id: 1, name: 'Иван', age: 20 },
+    { id: 2, name: 'Мария', age: 22 },
+    { id: 3, name: 'Алексей', age: 21 },
+    { id: 4, name: 'Марина', age: 19 },
+    { id: 5, name: 'Даша', age: 23 },
+    { id: 6, name: 'Глеб', age: 24 },
+    { id: 7, name: 'Дима', age: 18 },
+    { id: 8, name: 'Гриша', age: 20 },
+    { id: 9, name: 'Серафим', age: 21 },
+  ]);
+
+  const removePerson = (id) => {
+    let filteredPeople = people.filter(item => item.id !== id);
+    setPeople(filteredPeople);
+  };
+
+  return (
+    <ul>
+      {people.map((person) => (
+        <li key={person.id}>
+          {person.name}, {person.age} лет
+          <button onClick={() => removePerson(person.id)}>Удалить</button>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default List;
